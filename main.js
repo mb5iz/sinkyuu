f2f9f3f296f254d833707fb4955de99341e51683
mb5iz <kato.miu5iz@gmail.com>
2025-12-16 16:50:10 +0900

    1216

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 //読み込みを早くする//
 <script src="main.js" defer></script>
+
+$(function () {
+    $(".inview").on("inview", function (event, isInView) {
+        if (isInView) {
+            $(this).stop().addClass("is-show");
+        }
+    });
+});
