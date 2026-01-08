939ac394c8f91e305c7ec5b6924df54a33ea7bf6
mb5iz <kato.miu5iz@gmail.com>
2026-01-08 21:14:23 +0900

    0108

--- main.js
+++ main.js
@@ -8,3 +8,30 @@ $(function () {
         }
     });
 });
+
+const hamburger = document.getElementById('js-hamburger');
+const nav = document.querySelector('.gnav');
+const body = document.body;
+
+if (hamburger) {
+    hamburger.addEventListener('click', function () {
+        // メニューの開閉クラスを切り替え
+        nav.classList.toggle('is-active');
+
+        // 背景スクロール固定の切り替え
+        if (nav.classList.contains('is-active')) {
+            body.style.overflow = 'hidden';
+        } else {
+            body.style.overflow = '';
+        }
+    });
+}
+
+// メニュー内のリンクをクリックしたらメニューを閉じてスクロール固定を解除
+const navLinks = document.querySelectorAll('.gnav__item a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('is-active');
+        body.style.overflow = '';
+    });
+});
